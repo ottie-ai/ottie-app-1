67e0f18127e8d1f3b5b3f8d48360c0244657ec3c
Sonia Adamec <soniaadamec@Sonias-MacBook-Pro.local>
2025-11-25 17:56:55 +0000

    Remove template system

--- app/api/templates/route.ts
+++
@@ -1,9 +0,0 @@
-// API route for template operations
-
-import { NextResponse } from 'next/server'
-import { templates } from '@/lib/templates/config'
-
-export async function GET() {
-  return NextResponse.json({ templates })
-}
-
